d644fba68e54789b976eabce1f620d05e258cc0d
Yolo <g20180630105@gmail.com>
2025-03-15 17:31:54 +0800

    fix: 舍弃无用代码

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,3 @@ import coverImageAdjuster from "./components/CoverImageAdjuster/index.vue";
 
 export const CoverImageAdjuster = withInstall(coverImageAdjuster);
 export default CoverImageAdjuster;
-
-export * from "./components/CoverImageAdjuster/index.vue";
